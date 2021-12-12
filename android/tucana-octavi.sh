f8fb5a785180dbf8a3e888ea22a618f30cbe04df
Erik Drozina <erikdro41@gmail.com>
2021-12-12 19:47:09 +0100

    android: Upadte repos for octavi
    Signed-off-by: Erik Drozina <erikdro41@gmail.com>

--- android/tucana-octavi.sh
+++ android/tucana-octavi.sh
@@ -11,16 +11,16 @@ read -p "Folder name with source inside: " foldername
 if cd $android_working_dir$foldername; then
 
     echo -e "\nCloning Tucana DT\n"
-    git clone https://github.com/OctaviOS-Devices/device_xiaomi_tucana device/xiaomi/tucana
+    git clone https://github.com/erikdrozina/device_xiaomi_tucana device/xiaomi/tucana -b octavi-12
 
     echo -e "\nCloning Common DT\n"
-    git clone https://github.com/OctaviOS-Devices/device_xiaomi_tucana-common device/xiaomi/sm6150-common
+    git clone https://github.com/erikdrozina/device_xiaomi_sm6150-common device/xiaomi/sm6150-common -b octavi-12
 
     echo -e "\nCloning Kernel Tree\n"
-    git clone https://github.com/OctaviOS-Devices/kernel_xiaomi_tucana kernel/xiaomi/sm6150
+    git clone https://github.com/alibei/kernel_xiaomi_sm6150 kernel/xiaomi/sm6150 -b twelve
 
     echo -e "\nCloning Vendor Tree\n"
-    git clone https://gitlab.com/erikdrozina/android_vendor_xiaomi -b 12 vendor/xiaomi
+    git clone git@gitlab.com:erikdrozina/android_vendor_xiaomi.git -b 12 vendor/xiaomi
 
     echo -e "\nCloning Xiaomi Hardware\n"
     git clone https://github.com/erikdrozina/android_hardware_xiaomi -b 12 hardware/xiaomi
